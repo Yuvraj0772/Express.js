9556abd337c4645fbf23623797192cc6aee3a9ff
Yuvraj Singh Chouhan <yuvrajsinghchouhan9669@gmail.com>
2026-09-18 21:44:03 +0530

    fix bug

--- controller/userController.js
+++ controller/userController.js
@@ -1,10 +1,10 @@
 // this contains connecting your models and views
 
-import { userList } from "../model/userModel.js";
+// import { userList } from "../model/userModel.js";
 
-export function handleUser(req,resp){
-    const userData = userList();
+// export function handleUser(req,resp){
+//     const userData = userList();
 
-    console.log(userData);
-    resp.render('user',{users : userData});
-}
+//     console.log(userData);
+//     resp.render('user',{users : userData});
+// }
